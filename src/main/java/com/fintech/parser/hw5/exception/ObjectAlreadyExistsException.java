8ee02b0e5491ad3d8d3f1d197c123e7bfc037bd1
o.buivarenko@rosi-edu.ru <o.buivarenko@rosi-edu.ru>
2024-09-25 20:23:19 +0300

    changed hw5 and starter

--- src/main/java/com/fintech/parser/hw5/exception/ObjectAlreadyExistsException.java
+++ src/main/java/com/fintech/parser/hw5/exception/ObjectAlreadyExistsException.java
@@ -1,4 +1,4 @@
-package com.fintech.parser.hw5.exseption;
+package com.fintech.parser.hw5.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
