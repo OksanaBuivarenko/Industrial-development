6d0c48a1de10063d9e70cd95e009d8df29c7ec0f
o.buivarenko@rosi-edu.ru <o.buivarenko@rosi-edu.ru>
2024-11-13 23:20:45 +0300

    add gradle wrapper

---
+++ database/build.gradle.kts
@@ -0,0 +1,87 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+	java
+	jacoco
+	id("application")
+	id("org.sonarqube") version "5.1.0.4882"
+	id("org.springframework.boot") version "3.3.4"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.fintech"
+version = "0.0.1-SNAPSHOT"
+
+application {
+	mainClass.set("com.fintech.database.DatabaseApplication")
+}
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+sonar {
+	properties {
+		property("sonar.projectKey", "OksanaBuivarenko_Industrial-development")
+		property("sonar.organization", "oksanabuivarenko")
+		property("sonar.host.url", "https://sonarcloud.io")
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-mail")
+	implementation("io.jsonwebtoken:jjwt:0.9.1")
+	implementation("org.liquibase:liquibase-core")
+	implementation("org.hibernate.orm:hibernate-core:6.2.7.Final")
+	implementation("org.hibernate.orm:hibernate-jpamodelgen:6.2.7.Final")
+	compileOnly("org.projectlombok:lombok")
+	implementation("org.mapstruct:mapstruct:1.6.2")
+	runtimeOnly("org.postgresql:postgresql")
+	annotationProcessor("org.projectlombok:lombok")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.6.2")
+	annotationProcessor( "org.projectlombok:lombok-mapstruct-binding:0.2.0")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-aop")
+	implementation("com.google.guava:guava:11.0.2")
+	annotationProcessor("org.hibernate.orm:hibernate-jpamodelgen:6.2.7.Final")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
+	testImplementation("org.springframework.boot:spring-boot-testcontainers")
+	testImplementation("org.testcontainers:junit-jupiter")
+	testImplementation("org.testcontainers:postgresql")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+
+tasks.test {
+	finalizedBy(tasks.jacocoTestReport)
+	useJUnitPlatform()
+	testLogging {
+		exceptionFormat = TestExceptionFormat.FULL
+		events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+		showStandardStreams = true
+	}
+}
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
